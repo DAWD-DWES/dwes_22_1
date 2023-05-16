25e5bca0a2c5f6436c0a786da505abea25552304
Iván <jivanrod@gmail.com>
2023-05-16 15:32:19 +0200

    update

--- public/index.php
+++ public/index.php
@@ -8,27 +8,23 @@
  *   Si se solicita cerrar la sesión
  *     Destruyo la sesión
  *     Invoco la vista del formulario de login
- *   Sino (cualquier otro caso)
- *     Redirijo al cliente al script juego.php
- * Sino
- *  Si se está solicitando el formulario de login
- *     Invoco la vista del formulario de login
- *  Sino Si se pide procesar los datos del formulario
- *            Lee los valores del formulario
- *            Si los credenciales son correctos
- *               Redirijo al cliente al script de juego con una nueva partida
- *            Sino
- *               Invoco la vista del formulario de login con el flag de error
- * Sino si se solicita el formulario de registro
+ *    Sino redirección a juego para jugar una partida
+ *  Sino 
+ *   Si se pide procesar los datos del formulario
+ *       Lee los valores del formulario
+ *       Si los credenciales son correctos
+ *       Redirijo al cliente al script de juego con una nueva partida
+ *        Sino Invoco la vista del formulario de login con el flag de error
+ *    Sino si se solicita el formulario de registro
  *     Invoco la vista del formulario de registro
- * Sino si se solicita procesar el formulario de registro
+ *    Sino si se solicita procesar el formulario de registro
  *     Leo los datos
- *     Estalezco flags de error
+ *     Establezco flags de error
  *     Si hay errores
  *        Invoco la vista de formulario de registro  con información sobre los errores
- *     Sino persisto el usuario en la base de datos
+ *      Sino persisto el usuario en la base de datos
  *          Invoco la vista de formulario de login 
- * Sino (En cualquier otro caso)
+ *   Sino (En cualquier otro caso)
  *      Invoco la vista del formulario de login
  */
 require "../vendor/autoload.php";
@@ -37,13 +33,16 @@ use eftec\bladeone\BladeOne;
 use Dotenv\Dotenv;
 use App\BD\BD;
 use App\Modelo\Usuario;
-use App\Dao\UsuarioDao;
+use App\DAO\UsuarioDao;
 
 session_start();
-             
+
+// Inicializa el acceso a las variables de entorno
 
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->load();
+
+// Inicializa el acceso a las variables de entorno
 
 $views = __DIR__ . '/../vistas';
 $cache = __DIR__ . '/../cache';
@@ -55,18 +54,24 @@ function esNombreValido(string $nombre): bool {
     return preg_match("/^\w{3,15}$/", $nombre);
 }
 
-// 
+// Validación de la clave con 6 dígitos
 function esPasswordValido(string $clave): bool {
-    return (filter_var($clave, FILTER_VALIDATE_INT) && strlen($clave) == 6);
+    return preg_match("/^[0-9]{6}$/", $clave);
 }
 
+// Validación de correo que puede ser vacío o con e lformato correcto
 function esEmailValido(string $email): bool {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
+    return (empty($email) || filter_var($email, FILTER_VALIDATE_EMAIL));
 }
 
 // Establece conexión a la base de datos PDO
 try {
-    $bd = BD::getConexion();
+    $host = $_ENV['DB_HOST'];
+    $port = $_ENV['DB_PORT'];
+    $database = $_ENV['DB_DATABASE'];
+    $usuario = $_ENV['DB_USUARIO'];
+    $password = $_ENV['DB_PASSWORD'];
+    $bd = BD::getConexion($host, $port, $database, $usuario, $password);
 } catch (PDOException $error) {
     echo $blade->run("cnxbderror", compact('error'));
     die;
@@ -75,60 +80,53 @@ try {
 $usuarioDao = new UsuarioDao($bd);
 // Si el usuario ya está validado
 if (isset($_SESSION['usuario'])) {
-// Si se solicita cerrar la sesión
+    // Si se solicita cerrar la sesión
     if (isset($_REQUEST['botonlogout'])) {
-// Destruyo la sesión
+        // Destruyo la sesión
         session_unset();
         session_destroy();
         setcookie(session_name(), '', 0, '/');
-// Invoco la vista del formulario de login
+        // Invoco la vista del formulario de login
         echo $blade->run("formlogin");
         die;
-    } else { // Si se solicita una nueva partida
-        $usuario = $_SESSION['usuario'];
-// Redirijo al cliente al script de gestión del juego
+    } else {
+        // Redirijo al cliente al script de gestión del juego
         header("Location:juego.php?botonnuevapartida");
         die;
     }
 
-// Sino 
+    // Sino 
 } else {
-// Si se está solicitando el formulario de login
-    if (empty($_REQUEST) || isset($_REQUEST['botonlogin'])) {
-// Invoco la vista del formulario de login
-        echo $blade->run("formlogin");
-        die;
-// Si se está enviando el formulario de login con los datos
-    } elseif (isset($_REQUEST['botonproclogin'])) {
-// Lee los valores del formulario
+    if (isset($_REQUEST['botonproclogin'])) {
+        // Lee los valores del formulario
         $nombre = trim(filter_input(INPUT_POST, 'nombre', FILTER_UNSAFE_RAW));
         $clave = trim(filter_input(INPUT_POST, 'clave', FILTER_UNSAFE_RAW));
         $usuario = $usuarioDao->recuperaPorCredencial($nombre, $clave);
-// Si los credenciales son correctos
+        // Si los credenciales son correctos
         if ($usuario) {
             $_SESSION['usuario'] = $usuario;
-// Redirijo al cliente al script de juego con una nueva partida
+            // Redirijo al cliente al script de juego con una nueva partida
             header("Location:juego.php?botonnuevapartida");
             die;
         }
-// Si los credenciales son incorrectos
+        // Si los credenciales son incorrectos
         else {
-// Invoco la vista del formulario de login con el flag de error activado
+            // Invoco la vista del formulario de login con el flag de error activado
             echo $blade->run("formlogin", ['error' => true]);
             die;
         }
-// Si se solicita el formulario de registro
+        // Si se solicita el formulario de registro
     } elseif (isset($_REQUEST['botonregistro'])) {
         echo $blade->run("formregistro");
         die;
-// Si se solicita que se procese una petición de registro
+        // Si se solicita que se procese una petición de registro
     } elseif (isset($_REQUEST['botonprocregistro'])) {
         $nombre = trim(filter_input(INPUT_POST, 'nombre', FILTER_UNSAFE_RAW));
         $clave = trim(filter_input(INPUT_POST, 'clave', FILTER_UNSAFE_RAW));
         $email = trim(filter_input(INPUT_POST, 'email', FILTER_UNSAFE_RAW));
         $errorNombre = empty($nombre) || !esNombreValido($nombre);
         $errorPassword = empty($clave) || !esPasswordValido($clave);
-        $errorEmail = empty($email) || !esEmailValido($email);
+        $errorEmail = !esEmailValido($email);
         if ($errorNombre || $errorPassword || $errorEmail) {
             echo $blade->run("formregistro", compact('nombre', 'clave', 'email', 'errorNombre', 'errorPassword', 'errorEmail'));
             die;
@@ -144,8 +142,8 @@ if (isset($_SESSION['usuario'])) {
             die();
         }
     } else {
-// Invoco la vista del formulario de login
+        // Invoco la vista del formulario de login
         echo $blade->run("formlogin");
         die;
     }
-}    
+}
--- src/DAO/UsuarioDAO.php
+++ src/DAO/UsuarioDAO.php
@@ -2,33 +2,59 @@
 
 namespace App\DAO;
 
-use PDO;
+use \PDO;
 use App\Modelo\Usuario;
 
 class UsuarioDAO {
 
-    private $bd;
+    /**
+     * @var $bd Conexión a la Base de Datos
+     */
+    private PDO $bd;
 
-    function __construct($bd) {
+    /**
+     * Constructor de la clase UsuarioDAO
+     * 
+     * @param PDO $bd Conexión a la base de datos
+     * 
+     * @returns UsuarioDAO
+     */
+    public function __construct(PDO $bd) {
         $this->bd = $bd;
     }
+    
+     /**
+     * Inserta un objeto usuario en la tabla usuarios
+     * 
+     * @param Usuario $usuario Usuario a persistir 
+     * 
+     * @returns bool Resultado de la operación de inserción
+     */
 
-    function crea($usuario) {
+    function crea(Usuario $usuario): bool {
         $sql = "insert into usuarios (nombre, clave, email) values (:nombre, :clave, :email)";
         $sth = $this->bd->prepare($sql);
         $result = $sth->execute([":nombre" => $usuario->getNombre(), ":clave" => $usuario->getClave(), ":email" => $usuario->getEmail()]);
         return ($result);
     }
 
-    function modifica($usuario) {
+    public function modifica(Usuario $usuario) {
         
     }
 
-    function elimina($nombre) {
+    public function elimina(int $id) {
         
     }
 
-    function recuperaPorCredencial($nombre, $pwd) {
+    /**
+     * Recupera un objeto usuario dado su nombre de usuario y clave
+     * 
+     * @param string $nombre Nombre de usuario
+     * @param string $clave Clave del usuario
+     * 
+     * @returns Usuario que corresponde a ese nombre y clave o null en caso contrario
+     */
+    public function recuperaPorCredencial(string $nombre, string $pwd): ?Usuario {
         $this->bd->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
         $sql = 'select * from usuarios where nombre=:nombre and clave=:pwd';
         $sth = $this->bd->prepare($sql);
